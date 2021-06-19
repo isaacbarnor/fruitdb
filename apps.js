e50c2b87e8e81cf23af68a4e59fb978e6f28443b
isaacbarnor <isaacbarnor@gmail.com>
2021-06-19 11:39:20 +0000

    created fruitsDB and fruits collection created people collection

--- apps.js
+++ apps.js
@@ -1,26 +1,46 @@
-const { MongoClient } = require("mongodb");
-// Replace the uri string with your MongoDB deployment's connection string.
-// const uri =
-//   "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority";
+
+const mongoose = require("mongoose");
+
 const uri ="mongodb://localhost:27017";
-const client = new MongoClient(uri, {
+
+mongoose.connect(uri+"/fruitsDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-async function run() {
-  try {
-    await client.connect();
-    const database = client.db('fruitDB');
-    const fruits = database.collection('fruits');
+//Creating a fruitSchema
+const fruitSchema = new mongoose.Schema({
+  name : String,
+  ratings: Number,
+  market: String
+});
 
-    console.log("Connected successfully");
-    // Query for a movie that has the title 'Back to the Future'
-    // const query = { title: 'Back to the Future' };
-    // const fruit = await fruits.findOne(query);
-    // console.log(fruit);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// Creating a model from the Schema
+const Fruit = new mongoose.model('Fruit',fruitSchema);
+// Using the new Model to create a fruit -- a document in the Fruit collection in the FruitDB //
+const fruit = new Fruit({
+  name:"Aluguntugui",
+  ratings:5,
+  market: "Madina"
+})
+
+// fruit.save();
+//Creating a personSchema
+const personSchema = new mongoose.Schema({
+  name : String,
+  age: Number,
+  height: Number,
+  partners: Number
+});
+
+// Creating a model from the Schema
+const Person = new mongoose.model('Person',personSchema);
+// Using the new Model to create a fruit -- a document in the Fruit collection in the FruitDB //
+const person = new Person({
+  name : "Isaac Barnor",
+  age: 35,
+  height: 5.9,
+  partners: 12
+})
+
+// fruit.save();
+// person.save();
